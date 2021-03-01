9a5a50c563e7b5006f7950b50a1b25db11c293f6
Nathan Eloe <powerofazure@gmail.com>
2021-03-01 12:09:06 -0600

    Demo animation shenanigans

--- player-animation/Player.gd
+++ player-animation/Player.gd
@@ -30,10 +30,10 @@ func _ready():
 func _process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
-	#input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
-	#input_vector = input_vector.normalized()
+	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
+	input_vector = input_vector.normalized()
 	#print(input_vector)
-	#velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	
 	if input_vector != Vector2.ZERO:
 		animTree.set('parameters/Move/blend_position', input_vector)
@@ -47,7 +47,10 @@ func _process(delta):
 	
 		
 func _physics_process(delta): # change in time
-	velocity.y += GRAVITY * delta
-	print(velocity)
+	#velocity.y += GRAVITY * delta
+	#print(velocity)
 	velocity = move_and_slide(velocity) #update our position
 	# delta is known because _physics_process has ~constant delta
+
+func do_the_thing():
+	print("what is happening to me?")
